bb1acbcf9058aa36536a03b5f6c1cd5f2b5db1c6
Sean Tsai <seanseansean1223@hotmail.com>
2019-05-15 18:01:09 +0800

    Add input function with form

--- utils/forecast.js
+++ utils/forecast.js
@@ -3,13 +3,13 @@ const request = require('request')
 const forecast = (lat, long, callback) => {
     const url = 'https://api.darksky.net/forecast/' + process.env.DARKSKY_KEY + '/' + lat + ',' + long
 
-    request({url, json: true}, (error, { body }) => {
+    request({ url, json: true }, (error, { body }) => {
         if (error) {
-            callback('Unable to connect to weather service!')
+            callback('Unable to connect to weather service!', undefined)
         } else if (body.error) {
-            callback('Unable to find location')
+            callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.currently)
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
         }
     })
 
